b03c1a751b76159364d609d050906be88b0b7e94
anirban <anirban00537@gmail.com>
2025-02-04 00:03:24 +0600

    Add Type Definition for Pricing Component Props

--- components/landing/Pricing.tsx
+++ components/landing/Pricing.tsx
@@ -2,11 +2,25 @@ import React from "react";
 import { getPackages } from "@/services/subscription.service";
 import { PricingClient } from "./PricingClient";
 
+interface PricingProps {
+  packages: {
+    monthly: Array<{
+      id: string;
+      name: string;
+      description: string;
+      price: number;
+      variantId: string;
+      features: {
+        wordGeneration: {
+          limit: number;
+          description: string;
+        };
+        features: string[];
+      };
+    }>;
+  };
+}
 
-
-export async function Pricing() {
-  const response = await getPackages();
-  const packages = response.data.packages;
-
+export async function Pricing({ packages }: PricingProps) {
   return <PricingClient packages={packages} />;
 }
